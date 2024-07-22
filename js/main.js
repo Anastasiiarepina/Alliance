52059505a449640670e8b3e0c126048c941908e3
Anastasiiarepina <anastasiiarepina0706@gmail.com>
2024-07-22 20:08:27 +0300

    fix: logo svg sprite, navbar scroll height

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const navbar = document.querySelector(".navbar");
+const logo = document.querySelector(".logo-svg use");
+console.log(logo);
 window.addEventListener("scroll", () => {
-  this.scrollY > 1
-    ? navbar.classList.add("navbar-light")
-    : navbar.classList.remove("navbar-light");
+  if (this.scrollY > 1) {
+    navbar.classList.add("navbar-light");
+    logo.href.baseVal = "img/sprite.svg#logo";
+  } else {
+    navbar.classList.remove("navbar-light");
+    logo.href.baseVal = "img/sprite.svg#logo-light";
+  }
 });
