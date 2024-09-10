15d819d980a0b5d45ea61ceaa7507bf7e9cc1bca
Anastasiiarepina <anastasiiarepina0706@gmail.com>
2024-09-10 21:12:40 +0300

    add: page error

---
+++ error.php
@@ -0,0 +1,20 @@
+<?php 
+  $page_title='Политика конфиденциальности';
+  $second_image='second-header-none';
+  $is_autohim="second-avtohim-off";
+  include_once('header-page.php');?>
+<section class="section error">
+    <div class="container">
+      <div class="error-wrapper">
+        <span class="error-big-text">404</span>
+        <h4 class="error-title">Страница не найдена</h2>
+        <p class="error-text">
+          Мы не смогли найти страницу с таким адресом, попробуйте перейти на главную или напишите нам.
+        </p>
+        <a class="error-button button">Вернуться на главную</a>
+      </div>
+    </div>
+</section>
+<?php 
+  $is_cta='no-cta';
+  include_once('footer.php');?>
