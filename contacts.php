80727d611c90dc8260f5e44c4cc92bcf0abaab73
Anastasiiarepina <anastasiiarepina0706@gmail.com>
2024-09-10 19:26:02 +0300

    add: page contacts

---
+++ contacts.php
@@ -0,0 +1,12 @@
+
+<?php 
+$page_title='Контакты';
+$second_image='second-header-grey';
+$is_autohim="second-avtohim-off";
+include_once('header-page.php');
+$is_footer_logo='footer-logo no-logo';
+include_once('template-parts/contacts-block.php');?> 
+<div class="container">
+    <div class="contacts-map"></div>
+</div>
+<?php include_once('footer.php');?>
--- footer.php
+++ footer.php
@@ -56,46 +56,9 @@
     </section>
     <!-- /.cta -->
     <footer class="footer">
-      <div class="container">
-        <div class="footer-top">
-          <a href="#" class="footer-logo">
-            <svg class="logo-svg footer-logo">
-              <use href="img/sprite.svg#logo"></use>
-            </svg>
-          </a>
-
-          <a href="tel:+74996861014" class="footer-phone">+7 (999) 686-10-14</a>
-          <div class="footer-info">
-            <svg class="address-icon" width="12" height="16">
-              <use href="img/sprite.svg#address"></use>
-            </svg>
-            <address class="footer-info-address">
-              г. Москва, Холодильный пер. 4к1с8
-            </address>
-          </div>
-          <div class="footer-info">
-            <svg class="mail-icon" width="16" height="12">
-              <use href="img/sprite.svg#mail"></use>
-            </svg>
-            <a href="mailto:a.dragunov@tdaliance.ru" class="footer-info-mail">
-              a.dragunov@tdaliance.ru
-            </a>
-          </div>
-          <div class="footer-social">
-            <a href="#" class="footer-social-link">
-              <svg class="footer-social-icon" width="20" height="12">
-                <use href="img/sprite.svg#vk"></use>
-              </svg>
-            </a>
-            <a href="#" class="footer-social-link">
-              <svg class="footer-social-icon" width="16" height="16">
-                <use href="img/sprite.svg#insta"></use>
-              </svg>
-            </a>
-          </div>
-        </div>
-        <!-- /.footer-top -->
-      </div>
+      <?php 
+      $is_footer_logo='footer-logo';
+      include('template-parts/contacts-block.php');?>
       <hr class="footer-separator" />
       <div class="container">
         <div class="footer-bottom">
